32e572abd9af5bf479b45d2f6f4717cd757ad480
Denes Simon <symdrx@gmail.com>
2019-11-06 19:24:22 +0100

    Masodik commit

--- szamologep.py
+++ szamologep.py
@@ -8,4 +8,9 @@ driver.find_element(By.NAME, "a").send_keys("1")
 driver.find_element(By.NAME, "b").send_keys("2")
 
 
+header_text = driver.find_element(By.XPATH, "//h1").text
+print(header_text)
+assert  header_text == "Számológép"
+driver.find_element(By.XPATH, "//input[3]").click()
+#driver.find_element(By.ID, "gomb").click()
 
