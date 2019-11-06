fd24fda39385475df90f1e92d1e8b1346acadcdd
Denes Simon <symdrx@gmail.com>
2019-11-06 20:00:31 +0100

    ujabb comit

--- szamologep.py
+++ szamologep.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get("https://sdoktatas.github.io/first-project")
-driver.find_element(By.NAME, "a-input").send_keys("11")
-driver.find_element(By.NAME, "b-input").send_keys("22")
+driver.find_element(By.ID, "a-input").send_keys("11")
+driver.find_element(By.ID, "b-input").send_keys("22")
 
 
 header_text = driver.find_element(By.XPATH, "//h1").text
@@ -16,6 +16,6 @@ assert  header_text == "Számológép"
 
 driver.find_element(By.ID, "submit-button").click()
 
-eredmeny = driver.find_element(By.ID,"result-input").text
+eredmeny = driver.find_element(By.ID, "result-input").text
 
 assert  eredmeny == 33
