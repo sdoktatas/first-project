cb36af3a194c3d51ca111537837f1f2ecad39d17
Denes Simon <symdrx@gmail.com>
2019-11-08 17:53:18 +0100

    ora vegi valtozat

--- szamologep.py
+++ szamologep.py
@@ -4,9 +4,22 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get("https://sdoktatas.github.io/first-project")
-driver.find_element(By.ID, "a-input").send_keys("11")
-driver.find_element(By.ID, "b-input").send_keys("22")
+#driver.find_element(By.ID, "a-input").send_keys("11")
 
+first_number = int(input("Első szám? "))
+
+second_number = int(input("Második szám? "))
+
+expected = first_number + second_number
+
+first_input_field = driver.find_element(By.ID, "a-input")
+first_input_field.send_keys(first_number)
+#first_input_field.screenshot("first-input.png")
+
+#driver.find_element(By.ID, "b-input").send_keys("22")
+
+second_input_field = driver.find_element(By.ID, "b-input")
+second_input_field.send_keys(second_number)
 
 header_text = driver.find_element(By.XPATH, "//h1").text
 print(header_text)
@@ -18,4 +31,10 @@ driver.find_element(By.ID, "submit-button").click()
 
 eredmeny = driver.find_element(By.ID, "result-input").get_attribute("value")
 print("eredmény: " + eredmeny)
-assert  eredmeny == "33"
+
+driver.save_screenshot("result.png")
+#assert  eredmeny == "33"
+print(type(expected))
+print(type(eredmeny))
+
+assert int(eredmeny) == expected
