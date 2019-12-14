ed7acbf4fd4965fdf1fefecc921c74cb82f41349
Denes Simon <symdrx@gmail.com>
2019-12-14 18:33:04 +0100

    petclinic hw

--- petclinic_sql.py
+++ petclinic_sql.py
@@ -79,6 +79,50 @@ def create_owner(owner):
     cursor.close()
 
 
+def assert_owner_exists(name):
+    parts = name.split(" ")
+    first_name = parts[0]
+    last_name = parts[1]
+    cursor = conn.cursor()
+    owner_found = 0
+    cursor.execute("select count(*) from owners where first_name = %s and last_name= %s", (first_name, last_name))
+    for (count,) in cursor:
+        owner_found += count
+    cursor.close()
+    return owner_found > 0
+
+
+def delete_owner_by_id(id):
+    cursor = conn.cursor()
+    owner_found = 0
+    cursor.execute("delete from owners where id = %s", (id,))
+    conn.commit()
+
+
+def pet_names_younger_than(date):
+    names = []
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM pets p WHERE p.birth_date > %s", (date,))
+    for (name,) in cursor:
+        # names.append(f"{name}")
+        names.append(name)
+    cursor.close()
+    return names
+
+
+def get_owner_of(pet_name):
+    cursor = conn.cursor()
+    cursor.execute("SELECT OWNER_ID FROM PETS WHERE NAME = %s", (pet_name,))
+    for (found_id, ) in cursor:
+        owner_id = (found_id)
+    cursor.close()
+    owner = find_owner_by_id(owner_id)
+    return owner["name"]
+    # cursor.execute("SELECT first_name,last_name from owners WHERE id = (SELECT owner_id FROM pets WHERE NAME = %s)", (pet_name,))
+
+
+
+
 # print_owner_names()
 # print(list_owner_names())
 # print(list_owner_names_who_lives_in('Madison'))
@@ -86,7 +130,11 @@ def create_owner(owner):
 # print(count_owners_who_lives_in("Madison"))
 # create_owner_with("Lajcsi Lagzi", "Trombita utca 24.", "Csalyágaröcsöge", "555")
 # print_owner_names()
-owner = {"name": "Boborján Kis", "address": "Vastyúk u. 1..", "city": "Budapest", "telephone": "123456"}
-create_owner(owner)
-print_owner_names()
-
+# owner = {"name": "Boborján Kis", "address": "Vastyúk u. 1..", "city": "Budapest", "telephone": "123456"}
+# create_owner(owner)
+# print_owner_names()
+# print(assert_owner_exists("George Franklin"))
+# print(assert_owner_exists("Boborjan Kis"))
+# delete_owner_by_id(12)
+# print(pet_names_younger_than('2000-01-01'))
+# print(get_owner_of("Leo"))
