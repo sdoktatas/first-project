432e8150eda8defae0eeeab8dbb233733d4fa0f1
Denes Simon <symdrx@gmail.com>
2019-12-14 16:18:55 +0100

    petclinic hw

--- petclinic_sql.py
+++ petclinic_sql.py
@@ -43,7 +43,50 @@ def find_owner_by_id(id):
     return owner
 
 
+def count_owners_who_lives_in(city):
+    cursor = conn.cursor()
+    count_found = 0
+    cursor.execute("select count(*) from owners where city = %s", (city,))
+    for (count,) in cursor:
+        count_found = count
+    cursor.close()
+    return count_found
+
+
+def create_owner_with(name, address, city, telephone):
+    parts = name.split(" ")
+    first_name = parts[0]
+    last_name = parts[1]
+    cursor = conn.cursor()
+    cursor.execute("insert into owners (first_name,last_name, address,city, telephone) values(%s,%s,%s,%s,%s)",
+                   (first_name, last_name, address, city, telephone))
+    conn.commit()
+    cursor.close()
+
+
+def create_owner(owner):
+    name = owner["name"]
+    parts = name.split(" ")
+    first_name = parts[0]
+    last_name = parts[1]
+    address = owner["address"]
+    city = owner["city"]
+    telephone = owner["telephone"]
+    cursor = conn.cursor()
+    cursor.execute("insert into owners (first_name,last_name, address,city, telephone) values(%s,%s,%s,%s,%s)",
+                   (first_name, last_name, address, city, telephone))
+    conn.commit()
+    cursor.close()
+
+
 # print_owner_names()
 # print(list_owner_names())
 # print(list_owner_names_who_lives_in('Madison'))
-print(find_owner_by_id("1"))
+# print(find_owner_by_id("1"))
+# print(count_owners_who_lives_in("Madison"))
+# create_owner_with("Lajcsi Lagzi", "Trombita utca 24.", "Csalyágaröcsöge", "555")
+# print_owner_names()
+owner = {"name": "Boborján Kis", "address": "Vastyúk u. 1..", "city": "Budapest", "telephone": "123456"}
+create_owner(owner)
+print_owner_names()
+
