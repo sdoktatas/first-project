1c9ab55330605b48951d18caebd3f1fc1f2f7b52
Denes Simon <symdrx@gmail.com>
2019-12-14 13:28:23 +0100

    petclinic hw

--- petclinic_sql.py
+++ petclinic_sql.py
@@ -24,5 +24,26 @@ def list_owner_names():
     return names
 
 
+def list_owner_names_who_lives_in(city):
+    names = []
+    cursor = conn.cursor()
+    cursor.execute("SELECT first_name,last_name FROM owners WHERE city = %s", (city, ))
+    for (first_name, last_name) in cursor:
+        names.append(f"{first_name} {last_name}")
+    cursor.close()
+    return names
+
+
+def find_owner_by_id(id):
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT first_name,last_name, address, city, telephone FROM owners WHERE id = %s", (id,))
+    for (first_name,last_name, address, city, telephone) in cursor:
+        owner = {"name": first_name + " " + last_name, "address": address, "city": city, "telephone": telephone}
+    return owner
+
+
 # print_owner_names()
-print(list_owner_names())
+# print(list_owner_names())
+# print(list_owner_names_who_lives_in('Madison'))
+print(find_owner_by_id("1"))
