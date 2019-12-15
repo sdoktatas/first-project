5efb6463cf6ed2bb3dd33b0e39639e6b5c48cb97
Denes Simon <symdrx@gmail.com>
2019-12-15 10:18:22 +0100

    petclinic hw

--- petclinic_sql.py
+++ petclinic_sql.py
@@ -104,7 +104,6 @@ def pet_names_younger_than(date):
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM pets p WHERE p.birth_date > %s", (date,))
     for (name,) in cursor:
-        # names.append(f"{name}")
         names.append(name)
     cursor.close()
     return names
@@ -118,7 +117,6 @@ def get_owner_of(pet_name):
     cursor.close()
     owner = find_owner_by_id(owner_id)
     return owner["name"]
-    # cursor.execute("SELECT first_name,last_name from owners WHERE id = (SELECT owner_id FROM pets WHERE NAME = %s)", (pet_name,))
 
 
 def add_owner_to(owner_name, pet_name, birth_date, type_name):
@@ -131,7 +129,6 @@ def add_owner_to(owner_name, pet_name, birth_date, type_name):
         owner_id = ow_id
     cursor.execute("SELECT id from types WHERE NAME = %s", (type_name,))
     for (t_id,) in cursor:
-        # print(t_id)
         type_id = t_id
     cursor.close()
     cursor = conn.cursor()
@@ -139,6 +136,30 @@ def add_owner_to(owner_name, pet_name, birth_date, type_name):
     conn.commit()
     cursor.close()
 
+
+def get_pets_of(owner_name):
+    pets = []
+    parts = owner_name.split(" ")
+    first_name = parts[0]
+    last_name = parts[1]
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT p.NAME,p.birth_date,t.name FROM pets p inner join owners o ON p.owner_id = o.id inner join types t ON p.type_id = t.id where owner_id in (select id from owners WHERE o.first_name =%s AND o.last_name=%s)",
+        (first_name, last_name))
+    for (name, birth_date, type) in cursor:
+        pets.append({"name": name, "birth_date": str(birth_date), "type": type})
+    cursor.close()
+    return pets
+
+
+def print_visits_of(pet_name):
+    visits = []
+    cursor = conn.cursor()
+    cursor.execute("SELECT v.visit_date,v.description FROM pets p join visits v ON p.id = v.pet_id WHERE p.name = %s", (pet_name,))
+    for (visit_date, description) in cursor:
+        visits.append(f"{visit_date} {description}")
+    cursor.close()
+    print(visits)
 
 # print_owner_names()
 # print(list_owner_names())
@@ -155,4 +176,6 @@ def add_owner_to(owner_name, pet_name, birth_date, type_name):
 # delete_owner_by_id(12)
 # print(pet_names_younger_than('2000-01-01'))
 # print(get_owner_of("Leo"))
-add_owner_to("Lajcsi Lagzi","Karcsika","2015-06-20","cat")
+# add_owner_to("Lajcsi Lagzi","Karcsika","2015-06-20","cat")
+# print(get_pets_of("Lajcsi Lagzi"))
+# print_visits_of("Max")
